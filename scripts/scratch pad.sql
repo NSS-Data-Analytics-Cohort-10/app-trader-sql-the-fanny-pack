43c3c10f004d52f7f19c4a52ff6a9a7a9192f2e4
asialarkin <asiailarkin@gmail.com>
2023-10-21 10:04:39 -0500

    structured answer

--- scripts/scratch pad.sql
+++ scripts/scratch pad.sql
@@ -34,17 +34,28 @@ ORDER BY name;
  
  -----------------------------------------------------------------------------------------------------
  
--- PRICE
+-- (a). App Trader will purchase apps for 10,000 times the price of the app. For apps that are priced from free up to $1.00, the purchase price is $10,000. (pric))
 --- Price between 0-1, app rader will purchase for $10000, otherwise purchase price will be * 1000
-
 SELECT DISTINCT a.name, 
-CASE
-WHEN a.price::money < 1 THEN 10000
-ELSE a.price * 10000
-END AS app_store_price,
- a.rating AS app_store_rating,
- p.rating AS play_store_rating,
- p.price::money AS play_store_price
+	CASE
+	WHEN a.price::numeric < 1 THEN '10000'::money
+	ELSE a.price::money * 10000
+    END AS app_store_price,
+	a.rating AS app_store_rating,
+    p.rating AS play_store_rating,
+    p.price::money AS play_store_price
 FROM app_store_apps AS a
-INNER JOIN play_store_apps AS p ON a.name = p.name
+INNER JOIN play_store_apps AS p 
+ON a.name = p.name
 ORDER BY a.name;
+
+-- (b) Apps earn $5000 per month, per app store it is on, from in-app advertising and in-app purchases, regardless of the price of the app.
+
+
+-- (c) If App Trader owns rights to the app in both stores, it can market the app for both stores for a single cost of $1000 per month
+
+
+-- (d) For every half point that an app gains in rating, its projected lifespan increases by one year. In other words, an app with a rating of 0 can be expected to be in use for 1 year, an app with a rating of 1.0 can be expected to last 3 years, and an app with a rating of 4.0 can be expected to last 9 years.
+
+
+-- (e) App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same $1000 per month.
